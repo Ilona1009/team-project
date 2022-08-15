85830f5f90fa06f8e0d69393d03e1fa183af052c
Vovandir <106840463+Vovandir@users.noreply.github.com>
2022-08-15 13:02:54 +0300

    header.html

--- js/modal-header.js
+++ js/modal-header.js
@@ -1,11 +1,13 @@
 {
     const refs = {
       openModalBtn: document.querySelector("[data-modal-header-open]"), 
+      openModalBtn2: document.querySelector("[data-modal-nav-open]"), 
       closeModalBtn: document.querySelector("[data-modal-header-close]"),
       modal: document.querySelector("[data-modal-header]"),
     };
   
     refs.openModalBtn.addEventListener("click", toggleModal);
+    refs.openModalBtn2.addEventListener("click", toggleModal);
     refs.closeModalBtn.addEventListener("click", toggleModal);
   
     function toggleModal() {
