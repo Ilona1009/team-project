fad904a73736def67f1cdb8d988beffd6cf9b47e
Ilona1009 <105588398+Ilona1009@users.noreply.github.com>
2022-08-15 10:31:30 +0000

    Deploying to gh-pages from @ Ilona1009/team-project@78934795bec75bbe021b258d9f99138acb9e2b37 🚀

---
+++ index.8a6dde6d.js
@@ -0,0 +1,2 @@
+{var refs={openModalBtn:document.querySelector("[data-modal-header-open]"),openModalBtn2:document.querySelector("[data-modal-nav-open]"),closeModalBtn:document.querySelector("[data-modal-header-close]"),modal:document.querySelector("[data-modal-header]")};function toggleModal(){refs.modal.classList.toggle("is-hidden-header")}refs.openModalBtn.addEventListener("click",toggleModal),refs.openModalBtn2.addEventListener("click",toggleModal),refs.closeModalBtn.addEventListener("click",toggleModal)}
+//# sourceMappingURL=index.8a6dde6d.js.map
